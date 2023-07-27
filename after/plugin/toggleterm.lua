79f1383d4e820d6270fd91b9475513a6490484a7
warrenkeil <wdkeil@gmail.com>
2023-07-27 12:48:32 -0500

    fix toggleterm not closing

--- after/plugin/toggleterm.lua
+++ after/plugin/toggleterm.lua
@@ -1,2 +1,5 @@
-vim.api.nvim_set_keymap('n', '<F12>', ':ToggleTerm<CR>', {noremap = true, silent = true})
+local opts = {noremap = true, silent = true}
+vim.api.nvim_set_keymap('n', '<F12>', ':ToggleTerm<CR>', opts)
+vim.api.nvim_set_keymap('t', '<F12>', '<C-\\><C-n>:ToggleTerm<CR>', opts)
+
 
